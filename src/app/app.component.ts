1612e9b2b47efe253240bf8395085279e4f1a17f
Pedro <pedrogarrido971@gmail.com>
2023-06-21 10:52:28 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TagsService } from './tags.service';
+import { catchError, finalize } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,18 @@ export class AppComponent {
   title = 'HTTP-Articles';
   tagList: any = [];
   articlesList: any = [];
+  loading: boolean = false;
+  errorMsj: string = '';
+  lastTag: string = '';
+
 
   getTagList() {
-    this.service.getTags();
-    return this.service.tags;
+    this.loading = true;
+    this.service.getTags()
+      .pipe(
+        finalize(() => this.loading = false),
+        catchError(async (error) => this.errorMsj = error.name))
+      .subscribe(response => this.tagList = response.tags);
   }
 
   showTags() {
@@ -24,7 +34,27 @@ export class AppComponent {
   }
 
   getArticlesList(tag: string) {
-    this.service.getArticles(tag);
-    this.articlesList = this.service.articles.slice(0, 5);
+    this.loading = true;
+    this.lastTag = tag;
+    this.service.getArticles(tag)
+      .pipe(
+        finalize(() => this.loading = false),
+        catchError(async (error) => this.errorMsj = error.name))
+      .subscribe(response => this.articlesList = response.articles.slice(0, 5));
+  }
+
+
+  cancelError() {
+    this.errorMsj = '';
+  }
+
+  retry() {
+    this.errorMsj = '';
+    if (this.tagList.length == 0) {
+      this.getTagList();
+    }
+    else {
+      this.getArticlesList(this.lastTag);
+    }
   }
 }
--- src/app/tags.service.ts
+++ src/app/tags.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +8,15 @@ export class TagsService {
 
   constructor(private http: HttpClient) { }
 
-  tags: any = [];
-  articles: any = [];
   readonly baseUrl = "https://api.realworld.io/api";
 
   getTags() {
     const url = this.baseUrl + '/tags';
-    this.http.get<any>(url)
-      .pipe(catchError(error => { return ["Error"] }))
-      .subscribe(response => this.tags = response.tags)
+    return this.http.get<any>(url)
   }
 
   getArticles(tag: string) {
-    const url = this.baseUrl + `/articles?tag=${tag}`
-    this.http.get<any>(url)
-      .pipe(catchError(error => { return ["Error"] }))
-      .subscribe(response => this.articles = response.articles)
+    const url = this.baseUrl + `/articles?tag=${tag}`;
+    return this.http.get<any>(url)
   }
 }
